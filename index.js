e0474fecb2f8950209ad97008a582c976cfc650f
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-12-22 12:22:01 +0100

    Added exception and throwing if err

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var env = require('jjv')()
 var schema = require('./schema.json')
 
+var CCCFException = function(message, trace) {
+	this.message  = message
+	this.trace    = trace
+	this.toString = function() {
+		return this.message + '. Details in e.trace.'
+	}
+}
+
 env.addSchema('cccf', schema)
 env.addSchema('cccf-multiple', {
 	type  : 'array',
@@ -16,7 +24,10 @@ module.exports = {
 	schema : schema,
 	validate : function(config) {
 		if (!(typeof config == 'object')) config = JSON.parse(config)
-		if (config instanceof Array) return env.validate('cccf-multiple', config)
-		else return env.validate('cccf', config)
-	}
+		var _config = (config instanceof Array) ? config : [config]
+		var err = env.validate('cccf-multiple', _config)
+		if (err) throw new CCCFException('Invalid config', err)
+		return config
+	},
+	exception : CCCFException
 }
